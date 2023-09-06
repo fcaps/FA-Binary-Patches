2543792a6fc46a16784af43078052c064ee5460d
KionX <>
2023-09-06 23:07:50 +0500

    Adds base STARTUP support

--- hooks/HFAExtLoad.cpp
+++ hooks/HFAExtLoad.cpp
@@ -2,4 +2,12 @@
 asm(
   ".section h0; .set h0,0xA8EF63;"
   "JMP "QU(FAExtLoad)";"
+
+#ifdef STARTUP
+  ".section h1; .set h1,0x45A926;"
+  "push eax;"
+  "call "QU(STARTUP)";"
+  "pop eax;"
+  "ret 8;"
+#endif
 );
